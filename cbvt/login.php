3688777d5546e10b01a58b802e717eb4d9c6fb40
amitrai1095 <amit.rai1095@gmail.com>
2015-11-21 18:14:37 +0530

    AS

--- cbvt/login.php
+++ cbvt/login.php
@@ -1,16 +1,17 @@
 <?php
-	
+	echo "In Here";
 	$connection = new MongoCLient();
 	$database = $connection->CBVT;
 	$drivers = $database->createCollection('drivers');
-
+	
+	echo "database connection";
 	$licensenumber = $_POST["licensenumber"];
 	$passportnumber = $_POST["passportnumber"];
 
 	$driverinfo = array('licensenumber' => $licensenumber, 'passportnumber' => $passportnumber);
 
 	$driver = $drivers->find($driverinfo);
-
+	echo "Find Done";
 	foreach($driver as $tr){
 		$id = $tr["_id"];
 		header('Location: http://localhost/cbvt/home.php?id='.$id);
